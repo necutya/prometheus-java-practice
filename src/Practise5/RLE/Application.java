5a29f3a523bf216333fb357f102a23477b39b843
ne_cutya <art.lebedev2020@gmail.com>
2020-10-16 18:27:47 +0300

    Add gitignore file

--- src/Practise5/RLE/Application.java
+++ src/Practise5/RLE/Application.java
@@ -1,2 +1,31 @@
-package Practise5.RLE;public class Application {
+package Practise5.RLE;
+
+public class Application {
+    public static void main( String[] args )
+    {
+        if(args.length > 0) {
+            StringBuilder new_string = new StringBuilder();
+            String old_string = args[0] + ".";
+            int counter = 1;
+            for(int i=0; i < old_string.length() - 1; i++) {
+                if(old_string.charAt(i) == old_string.charAt(i + 1)) {
+                    counter++;
+                }
+                else {
+                    new_string.append(old_string.charAt(i));
+                    if(counter != 1) {
+                        new_string.append(counter);
+                        counter = 1;
+                    }
+                }
+
+                if(counter >= 9) {
+                    new_string.append(old_string.charAt(i));
+                    new_string.append(counter);
+                    counter = 0;
+                }
+            }
+            System.out.println(new_string);
+        }
+    }
 }
--- src/Practise5/RLE_decoder/Application.java
+++ src/Practise5/RLE_decoder/Application.java
@@ -1,4 +1,40 @@
 package Practise5.RLE_decoder;
 
-public class Applacation {
+public class Application {
+    public static void main( String[] args )
+    {
+        if(args.length > 0) {
+            String old_string = args[0] + ".";
+            StringBuilder new_string = new StringBuilder();
+
+            if (Character.isDigit(old_string.charAt(0))) {
+                System.out.println("");
+                return;
+            }
+
+            new_string.append(old_string.charAt(0));
+
+            for(int i=1; i < old_string.length() - 1; i++) {
+                char c1 = old_string.charAt(i);
+                char c2 = old_string.charAt(i + 1);
+                if (c1 == c2) {
+                    System.out.println("");
+                    return;
+                }
+                if(Character.isDigit(c1)){
+                    if(Character.isDigit(c2)) {
+                        System.out.println("");
+                        return;
+                    }
+                    for(int j=1; j < Character.getNumericValue(c1); j++) {
+                        new_string.append(old_string.charAt(i-1));
+                    }
+                }
+                else{
+                    new_string.append(c1);
+                }
+            }
+            System.out.println(new_string);
+        }
+    }
 }
